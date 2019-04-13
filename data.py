09c548cf36ceae332e5a516c99119bedce41b10e
Wen Han Hu <justinhu@Wens-MacBook-Pro.local>
2019-04-13 15:18:18 -0400

    update data.py

--- data.py
+++ data.py
@@ -11,16 +11,28 @@ def load_data():
 	# remove outliers
 	df = df[ (np.abs(stats.zscore(df.Quantity)) <= 3) & (np.abs(stats.zscore(df.Quantity)) <= 3)]
 	# Change the datatype of Customer ID from float to str
-	df['CustomerID']= df['CustomerID'].astype(str)
+	df['CustomerID']= df['CustomerID'].astype(float).astype(int).astype(str)
+	df['InvoiceNo'] = df['InvoiceNo'].astype(str)
 	# Separate Timstamp into Date and Time
 	df['Date'] = [d.date() for d in df['InvoiceDate']]
 	df['Time'] = [d.time() for d in df['InvoiceDate']]
-	df = df.drop(columns=['InvoiceDate'])
+
 	# Create Amount attributes
 	df['Amount'] = df['Quantity'] * df['UnitPrice']
+
+	# Create StockHead for product Category
+	df['StockHead'] = df['StockCode'].astype(str).str[0]
+
+	remove_head=['C','B','M','D','P']
+	for h in remove_head:
+		df = df[df['StockHead'] != h]
+	# drop unused columns
+	df = df.drop(columns=['InvoiceDate'])
+	df = df.drop(columns=['Country'])
 	return df
 
 
 if __name__ == "__main__":
 	df = load_data()
-	print (df.shape)
+	print ("DateFrame Shape:",df.shape)
+	print ("Columns:", df.columns)
