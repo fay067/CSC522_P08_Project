cd634dd1dd2fc64b8440ec251ba956d50b8a28aa
Wen Han Hu <justinhu@Wens-MacBook-Pro.local>
2019-04-20 15:57:35 -0400

    add typical rfm

--- rfm.py
+++ rfm.py
@@ -7,7 +7,7 @@ from tf_idf import *
 Author : Chu-Wen Chen, Ge Gao, Pei Liu, Wen-Han Hu
 '''
 
-def rfm(df,prod_type=None):
+def rfm(df,model_type=None):
     temp = df.copy()
     customers_rfm = pd.DataFrame()
     customers_rfm['CustomerID'] = temp['CustomerID'].unique()
@@ -15,10 +15,16 @@ def rfm(df,prod_type=None):
     customers_rfm['Recency'] = customers_rfm['CustomerID'].map((temp['Date'].max()-temp.groupby('CustomerID')['Date'].max()).to_dict().get)
     customers_rfm['Recency'] = customers_rfm['Recency'].dt.days
     
-    if ("ProdCate" in temp.columns) and prod_type:
+    if ("ProdCate" in temp.columns) and (model_type == 'TF-IDF'):
         col = "ProdCate"
+    elif model_type == 'StockID':
+        col = "StockHead"   
     else:
-        col = "StockHead"
+        temp = temp.groupby(['CustomerID'],as_index=False).sum()
+        temp = temp[['CustomerID','Amount']]
+        customers_rfm = pd.merge(customers_rfm,temp,on='CustomerID')
+        customers_rfm = customers_rfm.rename(index=str, columns={"Amount": "Monetary"})
+        return customers_rfm
         
     cluster = sorted(temp[col].unique())
     for i in cluster:
@@ -37,9 +43,12 @@ def rfm(df,prod_type=None):
     customers_rfm = pd.merge(customers_rfm,customer_spent,on='CustomerID')
     return customers_rfm
 
-def rfm_matrix(df):
-    df = df.drop(['Amount'], axis=1)
-    matrix = norm(df.iloc[:,1:]).values
+def rfm_matrix(df,model_type=None):
+    if model_type:
+        df = df.drop(['Amount'], axis=1)
+        matrix = norm(df.iloc[:,1:]).values
+    else:
+        matrix = norm(df.iloc[:,1:]).values
     return matrix
 
 def rfm_write_back(df,clusters):
